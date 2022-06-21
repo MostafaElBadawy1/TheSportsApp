5792499a57fdffa46a97e663560dc06fbf1db0d4
Mostafa <mostafa.elbadawy77777@gmail.com>
2022-06-22 00:01:12 +0200

    deleted unimportant files

---
+++ The Sports App/Scenes/LeagueDetailsScene/LeagueDetailsThreeCustomTableViewCells/LatestResultsTableViewCell/LatestResultsTableViewCell.swift
@@ -0,0 +1,57 @@
+//
+//  LatestResultsTableViewCell.swift
+//  The Sports App
+//
+//  Created by Mostafa Elbadawy on 21/06/2022.
+//
+
+import UIKit
+
+class LatestResultsTableViewCell: UITableViewCell {
+    @IBOutlet weak var latestResultsCollectionViewInTableViewCell: UICollectionView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        latestResultsCollectionViewInTableViewCell.delegate = self
+        latestResultsCollectionViewInTableViewCell.dataSource = self
+        latestResultsCollectionViewInTableViewCell.register(UINib(nibName: "LatestResultsCollectionViewCell", bundle: .main), forCellWithReuseIdentifier: "LatestResultsCollectionViewCell")
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
+extension LatestResultsTableViewCell: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        latestResultsCollectionViewInTableViewCell.deselectItem(at: indexPath, animated: false)
+    }
+    
+}
+extension LatestResultsTableViewCell: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        4
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = latestResultsCollectionViewInTableViewCell.dequeueReusableCell(withReuseIdentifier: "LatestResultsCollectionViewCell", for: indexPath) as! LatestResultsCollectionViewCell
+        cell.teamVsTeamLabel.text = "real x barca"
+        cell.scoreVsScoreLabel.text = "2 x 2"
+        cell.dateLabel.text = "12/5"
+        cell.timeLabel.text = "2:30"
+        return cell
+
+    }
+    
+    
+}
+// MARK: - CollectionViewDelegateFlowLayout
+extension LatestResultsTableViewCell: UICollectionViewDelegateFlowLayout{
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let leftAndRightPaddings: CGFloat = 1
+        let numberOfItemsPerRow: CGFloat = 2.0
+        let width = (collectionView.frame.width-leftAndRightPaddings)/numberOfItemsPerRow
+        return CGSize(width: width, height: width)
+    }
+}
--- The Sports App/Scenes/LeagueDetailsScene/ViewModel/LeagueDetailsViewModel.swift
+++ The Sports App/Scenes/LeagueDetailsScene/ViewModel/LeagueDetailsViewModel.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+class LeagueDetailsViewModel {
+    //var leagueArray: [League]?
+    //var filterSport: Sport?
+    let apiService: ApiService
+    init(apiService: ApiService = NetworkManager()) {
+        self.apiService = apiService
+    }
+    func fetch() async ->  [League]? {
+        let leaguesData = try? await apiService.fetchSports(endPoint: "api/v1/json/2/all_leagues.php", model: LeaguesModel.self)
+      
+        return leaguesData?.leagues
+    }
+}
