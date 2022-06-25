d77e9557b1128a23cf069bd0443845a0d31b4e93
Mostafa <mostafa.elbadawy77777@gmail.com>
2022-06-25 21:49:52 +0200

    fixed the crashing problem

--- The Sports App/NetworkingManager/NetworkingManager.swift
+++ The Sports App/NetworkingManager/NetworkingManager.swift
@@ -11,8 +11,9 @@ class NetworkManager: ApiService {
     static let shared = NetworkManager()
     private init() {}
     func fetchNetworkingData<T: Codable>(endPoint: String, model: T.Type) async throws -> T  {
+        let urlIfError = URL(string: UrlServices(endPoint: "api/v1/json/2/search_all_leagues.php?s=skiing").url)
         let url = URL(string: UrlServices(endPoint: endPoint).url)
-        let (data, _) = try await URLSession.shared.data(from: url!)
+        let (data,_) = try await URLSession.shared.data(from: url ?? urlIfError!)
         return try JSONDecoder().decode(T.self , from: data)
     }
 
